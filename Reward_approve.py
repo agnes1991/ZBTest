3cf2de6a182010eb496eda130ebb11fc2f1bf60d
anges1010 <rui_huang@outlook.com>
2016-04-28 18:11:09 +0800

    审核悬赏

--- Reward_approve.py
+++ Reward_approve.py
@@ -1,18 +1,30 @@
 #coding:utf-8
 from selenium import webdriver
 import sys
+from public import login
+import time
 
 driver = webdriver.Firefox()
 
 #登陆后台管理
-driver.get("http://www.oschina.org/home/login?goto_page=http%3A%2F%2Fadmin.oschina.net%2F")
-driver.find_element_by_id("f_email").clear()
-driver.find_element_by_id("f_email").send_keys("bei__mu@126.com")
-driver.find_element_by_id("f_pwd").clear()
-driver.find_element_by_id("f_pwd").send_keys("123456")
-driver.find_element_by_xpath("/html/body/div[3]/div[1]/div[1]/form/div[2]/table/tbody/tr[7]/td/input").click()
-driver.get("http://admin.oschina.org/")
+login.login(driver,'3')
+print "登陆成功"
 
+driver.implicitly_wait(3)
+
+driver.get("http://admin.oschina.org")
 #众包管理
 driver.find_element_by_xpath("/html/body/div[1]/div[1]/ul/li[3]/a").click()
 driver.find_element_by_xpath("/html/body/div[1]/div[2]/table/tbody/tr/td[1]/ul/li[2]/ol/li[1]/a").click()
+
+#查询到发布的悬赏
+driver.find_element_by_xpath("/html/body/div[1]/div[2]/table/tbody/tr/td[2]/div/h2/form/input[1]").clear()
+driver.find_element_by_xpath("/html/body/div[1]/div[2]/table/tbody/tr/td[2]/div/h2/form/input[1]").send_keys(u"悬赏自动化测试")
+driver.find_element_by_xpath("/html/body/div[1]/div[2]/table/tbody/tr/td[2]/div/h2/form/input[2]").submit()
+
+#审核通过
+driver.find_element_by_xpath("/html/body/div[1]/div[2]/table/tbody/tr/td[2]/div/table/tbody/tr[2]/td[8]/a[1]").click()
+driver.switch_to_alert().accept()
+print "悬赏通过审核"
+
+# driver.quit()
