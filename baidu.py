b916efc908b6908cca2925b570bf46b17d6e52f8
Rex Lee(李俊) <duguying2008@gmail.com>
2016-06-16 23:24:54 +0800

    use PhantomJS

--- baidu.py
+++ baidu.py
@@ -4,29 +4,19 @@ import sys
 from public import com
 
 
-driver = webdriver.Firefox()
-# driver.maximize_window()
+driver = webdriver.PhantomJS()
+driver.maximize_window()
 
 driver.get("https://www.oschina.net")
 
-div = driver.find_element_by_class_name("TodayNews")
-p1 = driver.find_element_by_class_name("p1")
-a = p1.find_element_by_tag_name("a")
+div = driver.find_element_by_id("ZB")
+a = div.find_element_by_tag_name("a")
 a.click()
 
 handle = com.get_window(driver, u"开源中国众包平台")
 # driver.switch_to_window(handle)
 
-aside = driver.find_element_by_tag_name("aside")
-rew = aside.find_elements_by_class_name("toolbox")
-a1 = rew[0].find_element_by_tag_name("a")
-a2 = rew[2].find_element_by_tag_name("a")
-print a1,a2
-a1.click()
-a2.click()
+menu = driver.find_element_by_class_name("menu")
+a_reward = menu.find_element_by_tag_name("a")
+a_reward.click()
 
-win2 = com.get_window(driver, u"前端二次开发")
-# driver.switch_to_window(win2)
-date1 = driver.find_element_by_class_name("publish-date")
-owner = date1.find_element_by_tag_name("a")
-owner.click()
