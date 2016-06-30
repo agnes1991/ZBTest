c56428c64a88f043676368cdcc138cc680ecda36
Agnes <rui_huang@outlook.com>
2016-06-30 18:43:41 +0800

    审核，定位alter

--- 2.0/approve_reward.py
+++ 2.0/approve_reward.py
@@ -9,5 +9,14 @@ driver = webdriver.Chrome()
 login.login(driver,'3')
 print("登陆成功")
 
-# driver.implicitly_wait(5)
-# driver.get("http://admin.oschina.org/")
+# 查找到发布的项目
+driver.find_element_by_link_text(u"众包管理").click()
+driver.find_element_by_link_text(u"待审核悬赏").click()
+driver.find_element_by_name("q").clear()
+driver.find_element_by_name("q").send_keys(u"发布一个悬赏测试全流程")
+driver.find_element_by_name("q").submit()
+
+# 审核项目
+driver.find_element_by_link_text(u"通过").click()
+# driver.switch_to_alert()
+driver.find_element_by_id("update-tag").click()
