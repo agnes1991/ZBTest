a8ba762ee72b45281d35b43ca8f93117e0eab04c
Agnes <rui_huang@outlook.com>
2016-06-30 17:33:18 +0800

    后台管理审核

---
+++ 2.0/approve_reward.py
@@ -0,0 +1,13 @@
+# coding:utf-8
+from selenium import webdriver
+from public import com
+from public import login
+
+driver = webdriver.Chrome()
+
+#发布者登陆
+login.login(driver,'')
+print("登陆成功")
+
+driver.implicitly_wait(5)
+driver.get("http://admin.oschina.org/")
