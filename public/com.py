845fe463e2b564718be9dc0057d0e9425428d474
Agnes <rui_huang@outlook.com>
2016-07-04 23:45:17 +0800

    删除废弃文件

--- public/com.py
+++
@@ -1,8 +0,0 @@
-#coding:utf-8
-from selenium import webdriver
-
-def get_window(driver,title):
-	for handle in driver.window_handles:
-		driver.switch_to_window(handle)
-		if title in driver.title:
-			return handle
